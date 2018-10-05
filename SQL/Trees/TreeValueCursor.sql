e67cb1d6b78985d87b5b419631323d63522a587d
Jay Dahlstrom <jamesd26@uw.edu>
2018-10-05 11:47:56 -0700

    Tree Value Comments
    Commented the tree value function

--- SQL/Trees/TreeValueCursor.sql
+++ SQL/Trees/TreeValueCursor.sql
@@ -42,36 +42,44 @@ FETCH NEXT FROM @TreeValueCursor INTO @TreeNumber, @DSH, @TreeType, @SpeciesRati
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
+
+	-- Handle NULL Values that would cause the calculations to fail. If a row contains a NULL value update the notes and skip the calculations
 	IF @DSH IS NULL OR @TreeType IS NULL OR @SpeciesRating IS NULL OR @Condition IS NULL OR @Contribution IS NULL OR @Placement IS NULL
 		SET @ValueNotes = 'More Information Required for Value'
 	ELSE
+
+		-- If the DSH is over 30" run one appraised trunk increase calculation and round to nearest int.  If under 30" run a different appraisal
 		IF @DSH > 30
 			SET @AppraisedTrunkIncrease = ROUND((((@DSH * @DSH * -.335) + (@DSH * 69.3) - 1087) - 7),0)
 		ELSE
 			SET @AppraisedTrunkIncrease = ROUND(((@DSH * @DSH * .785) - 7),0)
-
+		
+		-- Calculate basic tree cost.  Cost is different depending on tree type
 		IF @TreeType = 'Broadleaf Evergreen' OR @TreeType = 'Deciduous' OR @TreeType = 'Evergreen' OR @TreeType = 'Palm'
 			SET @BasicTreeCost = (@AppraisedTrunkIncrease * 72) + 480
 		ELSE
 			SET @BasicTreeCost = (@AppraisedTrunkIncrease * 57) + 380
 
-		SET @AssessedValue = ROUND((@BasicTreeCost /* * @SpeciesRating */ * @Condition * ((@Contribution + @Placement)/2)), 0)
+		-- Set the assessed value by multiplying basic tree cost by tree and species specific values.  Round to nearest int  
+		SET @AssessedValue = ROUND((@BasicTreeCost * @SpeciesRating * @Condition * ((@Contribution + @Placement)/2)), 0)
 
+		-- If the Assessed Value is less than $5000 round to nearest 10 otherwise round to nearest 100
 		IF @AssessedValue < 5000
 			SET @FinalAssessedValue = ROUND(@AssessedValue, -1)
 		ELSE
 			SET @FinalAssessedValue = ROUND(@AssessedValue, -2)
 	
+	-- Insert the Final Assessed Values into GroundsTreeValue table.  This table is accessed by the Public Trees Map (after migration to Public database via Python script)
 	INSERT INTO dbo.GroundsTreeValue (TreeNumber, TreeValue, Notes)
 	VALUES (@TreeNumber, @FinalAssessedValue, @ValueNotes)
 
-
-	PRINT cast(@TreeNumber AS NCHAR(50)) 
+	-- Console Log the insert values for error checking if needed
+	/* PRINT cast(@TreeNumber AS NCHAR(50)) 
 	PRINT cast(@FinalAssessedValue AS NCHAR(50))
-	PRINT @ValueNotes
+	PRINT @ValueNotes */
 	FETCH NEXT FROM @TreeValueCursor INTO @TreeNumber, @DSH, @TreeType, @SpeciesRating, @Condition, @Contribution, @Placement;
 END
 
-
+-- End the cursor
 CLOSE @TreeValueCursor
 DEALLOCATE @TreeValueCursor
