9480c42870ef50ba27c0ce160ad6cbf62bcbb0e1
Jay Dahlstrom <jamesd26@uw.edu>
2018-10-08 10:23:10 -0700

    Tree Value Edits
    Fixed issue in Final Assessed value formula that was causing the values to be zero.  Changed /100 to *.01 in ratings.  Most lilkely order of operations issues eventhough I used () to enclose statements.

--- SQL/Trees/TreeValueCursor.sql
+++ SQL/Trees/TreeValueCursor.sql
@@ -6,6 +6,10 @@
 -- Function is based on algorithm provided by Sara Shores.
 -- =============================================
 
+-- Figure out issue with values equally less than 1.
+
+
+
 -- Set working database and truncate the tree value table to start from scratch
 USE FacilitiesMaintenance
 GO
@@ -44,15 +48,15 @@ WHILE @@FETCH_STATUS = 0
 BEGIN
 
 	-- Handle NULL Values that would cause the calculations to fail. If a row contains a NULL value update the notes and skip the calculations
-	IF @DSH IS NULL OR @TreeType IS NULL OR @SpeciesRating IS NULL OR @Condition IS NULL OR @Contribution IS NULL OR @Placement IS NULL
+	IF @DSH IS NULL OR @TreeType IS NULL OR /* @SpeciesRating IS NULL OR */ @Condition IS NULL OR @Contribution IS NULL OR @Placement IS NULL
 		SET @ValueNotes = 'More Information Required for Value'
 	ELSE
 
 		-- If the DSH is over 30" run one appraised trunk increase calculation and round to nearest int.  If under 30" run a different appraisal
 		IF @DSH > 30
-			SET @AppraisedTrunkIncrease = ROUND((((@DSH * @DSH * -.335) + (@DSH * 69.3) - 1087) - 7),0)
+			SET @AppraisedTrunkIncrease = ROUND(((((@DSH * @DSH) * -.335) + (@DSH * 69.3) - 1087) - 7),0)
 		ELSE
-			SET @AppraisedTrunkIncrease = ROUND(((@DSH * @DSH * .785) - 7),0)
+			SET @AppraisedTrunkIncrease = ROUND((((@DSH * @DSH) * .785) - 7),0)
 		
 		-- Calculate basic tree cost.  Cost is different depending on tree type
 		IF @TreeType = 'Broadleaf Evergreen' OR @TreeType = 'Deciduous' OR @TreeType = 'Evergreen' OR @TreeType = 'Palm'
@@ -61,7 +65,7 @@ BEGIN
 			SET @BasicTreeCost = (@AppraisedTrunkIncrease * 57) + 380
 
 		-- Set the assessed value by multiplying basic tree cost by tree and species specific values.  Round to nearest int  
-		SET @AssessedValue = ROUND((@BasicTreeCost * @SpeciesRating * @Condition * ((@Contribution + @Placement)/2)), 0)
+		SET @AssessedValue = ROUND((@BasicTreeCost * /* (@SpeciesRating * .01) * */ (@Condition*.01) * (((85 + @Contribution + @Placement)/3)* .01)), 0) -- the 85 is for standard site rating
 
 		-- If the Assessed Value is less than $5000 round to nearest 10 otherwise round to nearest 100
 		IF @AssessedValue < 5000
@@ -74,11 +78,13 @@ BEGIN
 	VALUES (@TreeNumber, @FinalAssessedValue, @ValueNotes)
 
 	-- Console Log the insert values for error checking if needed
-	/*
+	
 	PRINT cast(@TreeNumber AS NCHAR(50)) 
 	PRINT cast(@FinalAssessedValue AS NCHAR(50))
+	PRINT cast(@AssessedValue AS NCHAR(50))
+	PRINT cast(@BasicTreeCost AS NCHAR(50))
 	PRINT @ValueNotes
-	*/
+	
 	FETCH NEXT FROM @TreeValueCursor INTO @TreeNumber, @DSH, @TreeType, @SpeciesRating, @Condition, @Contribution, @Placement;
 END
 
