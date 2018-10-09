b722ad7b3cebd517b0fae45d76edddcc7e1b2c05
Yurika Harada <26261926+ymharada@users.noreply.github.com>
2018-10-09 15:33:18 -0700

    Fixed Water Treatment Errors
    -ymharada commit

--- SQL/WaterTreatment/WaterTreatmentTables.sql
+++ SQL/WaterTreatment/WaterTreatmentTables.sql
@@ -25,27 +25,28 @@ CREATE TABLE WaterTreatment.dbo.WaterTreatmentClosedLoopsWaterTests
 	FacNum varchar(5) NOT NULL,
 	LoopType varchar(100) NOT NULL,
 	EntryGroup varchar (10) NOT NULL,
-	Conductivity int(4),
-	pH decimal(2,1),
-	MolybDate decimal(3,3),
-	Nitrite int(3), 
-	Azole decimal(2,1),
-	Copper decimal(1,5),
-	Iron decimal(3,3),
-	TotalBacteria int(3),
-	MakeupMeter decimal(6,3),
+	Conductivity int,
+	pH decimal(4,1),
+	MolybDate decimal(6,3),
+	Nitrite int, 
+	Azole decimal(4,1),
+	Copper decimal(5,5),
+	Iron decimal(6,3),
+	TotalBacteria int,
+	MakeupMeter decimal(9,3),
 	MildCopperCorrosionRate decimal,
 	MildSteelCorrosionRate decimal,
-	Leaks varchar(50),
+	Leaks varchar(10),
 	Notes varchar(500),
 	DataEntryUser varchar(50),
-	SurveyDate date() NOT NULL,
-	CreatedDate date(GETDATE()) NOT NULL,
-	Createdby varchar(50)
+	SurveyDate date NOT NULL,
+	CreateDate date,
+	Createdby varchar(50),
 
 
-
-
-	PRIMARY KEY (UniqueID),
+PRIMARY KEY (UniqueID),
  FOREIGN KEY (Leaks) REFERENCES WaterTreatment.dbo.WaterTreatmentDomainsLeaks(Leaks) ON UPDATE CASCADE ON DELETE CASCADE
-		);
+);
+
+ALTER TABLE WaterTreatment.dbo.WaterTreatmentClosedLoopsWaterTests ADD  CONSTRAINT [DF_EXAMPLE_date]  DEFAULT (getdate()) FOR CreateDate
+GO
