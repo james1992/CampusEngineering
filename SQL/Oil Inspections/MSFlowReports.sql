44f9442f927db5d727b0bb82bc54fbcc9015bb9d
Yurika Harada <26261926+ymharada@users.noreply.github.com>
2019-02-19 08:35:33 -0800

    Create MSFlowReports.sql

---
+++ SQL/Oil Inspections/MSFlowReports.sql
@@ -0,0 +1,29 @@
+-- =============================================
+-- Author:		Yurika Harada
+-- Create date: 2/19/2019
+-- Description:	This TSQL is designed to create a view
+-- for the Oil Inspection GIS that will be updated daily.
+-- The view is used for MS Flow to send Joe daily updates
+-- on any sites that need attention.
+-- =============================================
+
+-- Define the working database
+
+USE [EngineeringServices]
+GO
+
+CREATE VIEW [dbo].[ViewOilInspectionsMSFlowDailyReport]
+AS
+SELECT  dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.SiteID AS SiteID, dbo.ENVIRONMENTALOILSPILLPREVENTION.Type AS SiteType, 
+dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.NeedsAttention, dbo.ENVIRONMENTALOILSPILLPREVENTION.Content, 
+dbo.ENVIRONMENTALOILSPILLPREVENTION.Description AS SiteDescription, dbo.ENVIRONMENTALOILSPILLPREVENTION.Capacity AS Capacity, 
+dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.Notes,dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.created_date, 
+dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.UserID
+FROM ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS
+LEFT JOIN dbo.ENVIRONMENTALOILSPILLPREVENTION
+ON dbo.ENVIRONMENTALOILSPILLPREVENTION.SiteID = ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.SiteID
+
+
+
+WHERE GETDATE()= dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.created_date AND
+dbo.ENVIRONMENTALOILSPILLPREVENTIONINSPECTIONS.NeedsAttention='Yes'
