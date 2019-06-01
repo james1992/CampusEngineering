d430fb6fbdca6304b30eea05c62b5db3a92bf08b
Jay Dahlstrom <jamesd26@uw.edu>
2019-05-31 17:17:03 -0700

    Cleaned up water treatment report update script

--- SQL/WaterTreatment/WaterTreatmentCondenserWaterReport.sql
+++ SQL/WaterTreatment/WaterTreatmentCondenserWaterReport.sql
@@ -1,19 +1,19 @@
 -- =============================================
 -- Author:		Jay Dahlstrom
--- Create date: 1/28/2019
+-- Create date: 2/22/2019
 -- Description:	This TSQL is designed to be run through
 -- SQL Server agent on an hourly basis to update the
--- Confidence Test Maintenance tables.  This replaces
--- all of the views that previously supported the web maps
--- with only the two tables produced by this script.
+-- Water Treatment Condenser Water Report table.  
+-- This tables contains the properly formatted information
+-- for the monthly inspection reports email that is
+-- produced by MS Flow.
 -- =============================================
 
 -- Define the working database
 
 USE FacilitiesMaintenance
 
--- Create a temporary table to hold confidence test maintenance most recent data
--- Table is automatically purged from tempdb after session, drop statement is there just in case to prevent errors.
+-- Remove all of the old records in preparation for data load
 
 TRUNCATE TABLE WaterTreatmentCondenserWaterReport
 GO
@@ -30,7 +30,8 @@ SELECT      BuildingSystem,
 			AvgORP, 
 			YearAndMonth 
 FROM  (SELECT        
-			CONCAT(BaseComponents.dbo.ViewUniversityBuildings.FacilityName, '-', dbo.WaterTreatmentCondenserWaterTests.LoopType) AS BuildingSystem, AVG(dbo.WaterTreatmentCondenserWaterTests.Conductivity) AS AvgConductivity, 
+			CONCAT(BaseComponents.dbo.ViewUniversityBuildings.FacilityName, '-', dbo.WaterTreatmentCondenserWaterTests.LoopType) AS BuildingSystem, 
+			AVG(dbo.WaterTreatmentCondenserWaterTests.Conductivity) AS AvgConductivity, 
 			CAST(ROUND(AVG(dbo.WaterTreatmentCondenserWaterTests.TRASAR), 2, 1) AS decimal(18, 2)) AS AvgTRASAR, 
 			CAST(ROUND(AVG(dbo.WaterTreatmentCondenserWaterTests.pH), 2, 1) AS decimal(18, 2)) AS AvgpH, 
 			CAST(ROUND(AVG(dbo.WaterTreatmentCondenserWaterTests.FreeChlorine), 2, 1) AS decimal(18, 2)) AS AvgFreeChlorine, 
@@ -41,6 +42,6 @@ FROM  (SELECT
 FROM        dbo.WaterTreatmentCondenserWaterTests LEFT OUTER JOIN
 				BaseComponents.dbo.ViewUniversityBuildings ON dbo.WaterTreatmentCondenserWaterTests.FacNum = BaseComponents.dbo.ViewUniversityBuildings.FacilityNumber
 WHERE		(BaseComponents.dbo.ViewUniversityBuildings.FacilityName IS NOT NULL)
-GROUP BY	CONCAT(BaseComponents.dbo.ViewUniversityBuildings.FacilityName, '-', dbo.WaterTreatmentCondenserWaterTests.LoopType), CONCAT(YEAR(dbo.WaterTreatmentCondenserWaterTests.SurveyDate), '-', 
-				FORMAT(dbo.WaterTreatmentCondenserWaterTests.SurveyDate, 'MM'))) AS innertable
+GROUP BY	CONCAT(BaseComponents.dbo.ViewUniversityBuildings.FacilityName, '-', dbo.WaterTreatmentCondenserWaterTests.LoopType), 
+			CONCAT(YEAR(dbo.WaterTreatmentCondenserWaterTests.SurveyDate), '-', FORMAT(dbo.WaterTreatmentCondenserWaterTests.SurveyDate, 'MM'))) AS innertable
 GO
